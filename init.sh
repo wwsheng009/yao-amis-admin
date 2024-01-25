25385ffc1eecc562928358af96b97a0d24736fea
wwsheng009 <vincentwwsheng@gmail.com>
2024-01-25 13:35:16 +0800

    Refactor database and container configurations
    Update database connection details and Docker container config to use
    hostnames and environment variables. Add a conditional check for the
    .env file in the initialization script to enable smooth migrations.
    These changes enhance maintainability and portability.
    
    #ISSUE-123

--- init.sh
+++ init.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+# Path to the .env file
+ENV_FILE=".env"
+
+# Check if the .env file exists
+if [ -f "$ENV_FILE" ]; then
+    echo ".env file found. Running 'yao migrate'..."
+    # Run yao migrate
+    /usr/local/bin/yao migrate
+fi
+
+
 /usr/local/bin/yao start
