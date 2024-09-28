331137a8d09dcf03651967ee02ab581821dd4eba
wwsheng009 <vincentwwsheng@gmail.com>
2024-09-28 22:54:33 +0800

    add email function(20%)

---
+++ scripts/__types/email.ts
@@ -0,0 +1,31 @@
+/**
+ * Email Message for email plugin
+ */
+export interface EmailMessage {
+  /** email server config */
+  account: {
+    server: string;
+    /** email server port */
+    port: number;
+    username: string;
+    password: string;
+    /** stmp for send email,imap for receive email*/
+    type: 'stmp' | 'imap';
+  };
+  /** messages for send */
+  messages?: {
+    from: string;
+    to: {
+      name?: string;
+      address: string;
+    }[];
+    cc?: {
+      name?: string;
+      address: string;
+    }[];
+    subject: string;
+    body: string;
+    /** attachment file path */
+    attachments?: string[];
+  }[];
+}
---
+++ scripts/app/email/test.ts
@@ -0,0 +1,53 @@
+import { EmailMessage } from '@yao/email';
+import { Process } from '@yao/yao';
+
+//yao run scripts.app.email.test.send
+function send(title: string) {
+  const username = Process('utils.env.Get', 'EMAIL_USERNAME');
+  const to = Process('utils.env.Get', 'EMAIL_TO');
+  const password = Process('utils.env.Get', 'EMAIL_PASSWORD');
+
+  const message = {
+    account: {
+      server: 'smtp.qq.com',
+      port: 587,
+      username: username,
+      password: password,
+      type: 'stmp'
+    },
+    messages: [
+      {
+        from: username,
+        to: [{ address: to }, { name: 'vincent1', address: to }],
+        cc: [{ address: username }],
+        subject: title || '小佩奇',
+        body: '<h1>新年快乐</h1>',
+        attachments: [
+          './data/upload/public/20240925/3351df068659b381b93403404e3f1b92.jpeg',
+          './data/upload/public/20240928/d8e4992057f9.jpeg'
+        ]
+      }
+    ]
+  } as EmailMessage;
+  const resp = Process('plugins.email.send', message);
+  console.log(resp);
+}
+
+//yao run scripts.app.email.test.receive
+function receive() {
+  const username = Process('utils.env.Get', 'EMAIL_USERNAME');
+  const to = Process('utils.env.Get', 'EMAIL_TO');
+  const password = Process('utils.env.Get', 'EMAIL_PASSWORD');
+
+  const message = {
+    account: {
+      server: 'imap.qq.com',
+      port: 993,
+      username: username,
+      password: password,
+      type: 'imap'
+    }
+  } as EmailMessage;
+  const resp = Process('plugins.email.receive', message);
+  console.log(resp);
+}
