1c6bb8e52acf2dd885ece9066f7c67ab777c0304
wwsheng009 <vincentwwsheng@gmail.com>
2024-10-06 10:56:53 +0800

    add initcheck for startup hook

--- scripts/hook.ts
+++ scripts/hook.ts
@@ -1,5 +1,5 @@
-import { importDBModelsToCache } from '@scripts/system/model';
-import { checkEmailAccount } from '@scripts/app/email/client';
+import { Process } from '@yaoapps/client';
+
 /**
  * 在应用启动时自动加载模型，这个特性需要修改版的yao才支持，非官方功能
  *
@@ -13,12 +13,36 @@ import { checkEmailAccount } from '@scripts/app/email/client';
 export function startup(config: object, action: string) {
   // 生产环境才自动加载，开发环境，开发人员手动处理
   console.log('startup action', action);
-  if (action != 'reload') {
+  if (action == 'production') {
+    initCheck();
     // console.log('config', config);
     console.log('启动-加载所有数据库模型到缓存');
-    importDBModelsToCache();
-    //检查邮件配置
-    checkEmailAccount();
+
+    Process('scripts.system.model.importDBModelsToCache');
+
+    Process('scripts.app.email.client.checkEmailAccount');
+  }
+}
+export function initCheck() {
+  if (!Process('schemas.default.tableexists', 'ddic_model')) {
+    console.log('初始化系统模型：ddic.model/ddic.model.column');
+    Process('models.ddic.model.migrate');
+    Process('models.ddic.model.column.migrate');
+    // return;
+  }
+  //检查邮件配置
+  if (!Process('schemas.default.tableexists', 'app_email_account')) {
+    console.log('初始化邮件配置模型:app.email.client');
+    Process('models.app.email.account.migrate');
+  }
+  if (!Process('schemas.default.tableexists', 'app_email_message')) {
+    console.log('初始化邮件消息模型:app.email.message');
+    Process('models.app.email.message.migrate');
+  }
+
+  if (!Process('schemas.default.tableexists', 'system_amis_page')) {
+    console.log('初始化Amis页面配置表:system.amis.page');
+    Process('models.system.amis.page.migrate');
   }
 }
 
