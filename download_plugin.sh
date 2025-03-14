15b8109f6dabc4cade9b1f855bec11e8d2acc892
wwsheng009 <vincentwwsheng@gmail.com>
2025-03-14 11:28:33 +0800

    refactor(download_plugin.sh): 修改插件及相关文件的安装路径为相对路径，优化文件操作逻辑

--- download_plugin.sh
+++ download_plugin.sh
@@ -7,27 +7,27 @@ install_plugins() {
     local ARCH=$1
 
     # 创建插件目录
-    mkdir -p /plugins
+    mkdir -p plugins
 
     # 下载并安装amis-editor
-    mkdir -p /public/amis-editor
-    curl -fsSL "https://github.com/wwsheng009/amis-editor-yao/releases/download/1.1.0/amis-editor-1.1.0.zip" -o /public/amis-editor/latest.zip
-    unzip -q /public/amis-editor/latest.zip -d /public/amis-editor/
-    rm /public/amis-editor/latest.zip
+    mkdir -p public/amis-editor
+    curl -fsSL "https://github.com/wwsheng009/amis-editor-yao/releases/download/1.1.0/amis-editor-1.1.0.zip" -o public/amis-editor/latest.zip
+    unzip -q public/amis-editor/latest.zip -d public/amis-editor/
+    rm public/amis-editor/latest.zip
 
     # 下载并安装soy-admin
-    mkdir -p /public/soy-admin
-    curl -fsSL "https://github.com/wwsheng009/soybean-admin-amis-yao/releases/download/0.10.4/soy-yao-admin-0.10.4.zip" -o /public/soy-admin/latest.zip
-    unzip -q /public/soy-admin/latest.zip -d /public/soy-admin/
-    rm /public/soy-admin/latest.zip
+    mkdir -p public/soy-admin
+    curl -fsSL "https://github.com/wwsheng009/soybean-admin-amis-yao/releases/download/0.10.4/soy-yao-admin-0.10.4.zip" -o public/soy-admin/latest.zip
+    unzip -q public/soy-admin/latest.zip -d public/soy-admin/
+    rm public/soy-admin/latest.zip
 
     # 下载插件
-    curl -fsSL "https://github.com/wwsheng009/yao-plugin-command/releases/download/command-linux-plugin/command-linux-${ARCH}.so" -o /plugins/command.so
-    curl -fsSL "https://github.com/wwsheng009/yao-plugin-psutil/releases/download/psutil-linux-plugin/psutil-linux-${ARCH}.so" -o /plugins/psutil.so
-    curl -fsSL "https://github.com/wwsheng009/yao-plugin-email/releases/download/email-linux-plugin/email-linux-${ARCH}.so" -o /plugins/email.so
+    curl -fsSL "https://github.com/wwsheng009/yao-plugin-command/releases/download/command-linux-plugin/command-linux-${ARCH}.so" -o plugins/command.so
+    curl -fsSL "https://github.com/wwsheng009/yao-plugin-psutil/releases/download/psutil-linux-plugin/psutil-linux-${ARCH}.so" -o plugins/psutil.so
+    curl -fsSL "https://github.com/wwsheng009/yao-plugin-email/releases/download/email-linux-plugin/email-linux-${ARCH}.so" -o plugins/email.so
 
     # 设置执行权限
-    chmod +x /plugins/*.so
+    chmod +x plugins/*.so
 }
 
 # 架构自动检测
